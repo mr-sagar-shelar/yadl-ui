6afd31a281b57b83a8d58dd86e5f514f1272a9e4
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-03-22 11:30:27 +0530

    Added theme selection drodpdown

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,16 @@
 const Button = () => {
   return (
     <>
-      <button type="button" className="btn btn-primary">
-        Click Me!
+      <button type="button" className="btn btn-soft">
+        Default
       </button>
+      <button className="btn btn-soft btn-primary">Primary</button>
+      <button className="btn btn-soft btn-secondary">Secondary</button>
+      <button className="btn btn-soft btn-accent">Accent</button>
+      <button className="btn btn-soft btn-info">Info</button>
+      <button className="btn btn-soft btn-success">Success</button>
+      <button className="btn btn-soft btn-warning">Warning</button>
+      <button className="btn btn-soft btn-error">Error</button>
     </>
   );
 };
