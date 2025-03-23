cb37a7f6f6f13f09d1187e3812b7d2f31e55bb59
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-03-23 10:00:32 +0530

    Completed icon name display

--- scripts/scrapUndraw.js
+++ scripts/scrapUndraw.js
@@ -1,4 +1,4 @@
-const { chromium } = require("playwright");
+import { chromium } from "playwright";
 (async () => {
   const browser = await chromium.launch({
     headless: false,
@@ -9,15 +9,30 @@ const { chromium } = require("playwright");
   const page = await context.newPage();
   await page.goto("https://undraw.co/illustrations");
 
-  // const appContainers = await page.locator(".appcontainer");
-  // const iconsCount = await appContainers.nth(2).locator(".bg-gray-50").count();
-
   const allIllustrations = await page
     .locator(".appcontainer")
     .nth(2)
     .locator(".bg-gray-50");
   const illustrationCount = await allIllustrations.count();
   console.log(`illustrations = ${illustrationCount}`);
+
+  // const texts = await allIllustrations.allTextContents();
+  // console.log(`texts = ${texts}`);
+
+  for (let i = 0; i < illustrationCount; i++) {
+    const currentIcon = await allIllustrations.nth(i);
+    const iconName = await currentIcon.allTextContents();
+    console.log(`${iconName}`);
+
+    // const temp = await allIllustrations.nth(i).locator(".injected-svg").count();
+    // console.log(`${temp}`);
+    // const svgContent = await currentIcon.querySelector(".injected-svg");
+    // console.log(`${svgContent}`);
+  }
+
+  // const firstIcon = await allIllustrations.nth(2).locator("svg").count();
+  // console.log(`firstIcon = ${firstIcon}`);
+
   // await page.waitForTimeout(1000);
   // await page.getByText("Next").click();
   await page.waitForTimeout(1000);
