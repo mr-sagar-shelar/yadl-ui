3d94b4a60ce12dab52369f5465aac9cd3842ab65
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-03-29 17:19:34 +0530

    Added drag and drop example

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,50 +1,132 @@
-import { useState, useCallback } from "react";
+import { useCallback, useRef } from "react";
 import {
   ReactFlow,
   Controls,
   Background,
-  applyNodeChanges,
-  applyEdgeChanges,
-  Node,
-  Edge,
   BackgroundVariant,
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  useReactFlow,
+  type Node,
+  type Edge,
+  type FitViewOptions,
+  // type OnConnect,
+  // type OnNodesChange,
+  // type OnEdgesChange,
+  // type OnNodeDrag,
+  // type NodeTypes,
+  type DefaultEdgeOptions,
 } from "@xyflow/react";
 import { YadlNodeTypes } from "./Nodes";
+import Sidebar from "./Sidebar";
+import { DnDProvider, useDnD } from "./DnDContext";
+import "./xy-themes.css";
 
 export type YadlPreviewProps = {
   nodes: Node[];
   edges: Edge[];
 };
 
-const YadlPreview = (props: YadlPreviewProps) => {
-  const [nodes, setNodes] = useState(props.nodes);
-  const [edges, setEdges] = useState(props.edges);
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
 
-  const onNodesChange = useCallback(
-    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const YadlPreview = (props: YadlPreviewProps) => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(props.nodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(props.edges);
+  const { screenToFlowPosition } = useReactFlow();
+  const [type] = useDnD();
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
     [],
   );
-  const onEdgesChange = useCallback(
-    (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [],
+
+  // const onNodesChange = useCallback(
+  //   (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+  //   [],
+  // );
+  // const onEdgesChange = useCallback(
+  //   (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+  //   [],
+  // );
+
+  // const onConnect = useCallback(
+  //   (params) => setEdges((eds) => addEdge(params, eds)),
+  //   [],
+  // );
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      if (!type) {
+        return;
+      }
+
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [screenToFlowPosition, type],
   );
+
   return (
     <div style={{ height: "100%", width: "100%" }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        fitView
-        proOptions={{ hideAttribution: true }}
-        defaultEdgeOptions={{ animated: true }}
-        nodeTypes={YadlNodeTypes}
-      >
-        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-        <Controls />
-      </ReactFlow>
+      <div className="dndflow">
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+            fitViewOptions={fitViewOptions}
+            defaultEdgeOptions={defaultEdgeOptions}
+            proOptions={{ hideAttribution: true }}
+            nodeTypes={YadlNodeTypes}
+          >
+            <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+            <Controls />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </div>
     </div>
   );
 };
 
-export default YadlPreview;
+export default (props: YadlPreviewProps) => (
+  <ReactFlowProvider>
+    <DnDProvider>
+      <YadlPreview {...props} />
+    </DnDProvider>
+  </ReactFlowProvider>
+);
