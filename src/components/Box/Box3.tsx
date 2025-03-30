2dae30dcbfd3681e8c5a581bcd06f320c0a24446
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-03-30 17:33:35 +0530

    Added box3

---
+++ src/components/Box/Box3.tsx
@@ -0,0 +1,37 @@
+import { getIcon } from "@utils";
+import { useMemo } from "react";
+
+export type BoxProps = {
+  width?: number;
+  height?: number;
+  text: string;
+  icon: string;
+  title: string;
+};
+
+const Box = (props: BoxProps) => {
+  const { width = 200, height = 200, text, icon, title } = props;
+  const SvgIcon = useMemo(() => getIcon(icon), [icon]);
+  return (
+    <>
+      <div
+        className="text-primary border border-base-content grid grid-col-2 justify-center p-4 gap-5 rounded-2xl bg-primary-content"
+        style={{ width, height }}
+      >
+        <div className="col-span-2 text-lg font-bold capitalize rounded-md">
+          {title}
+        </div>
+        <div className="col-span-2 rounded-md">{text}</div>
+        <div className="col-span-1">
+          <button className="rounded-md bg-slate-300 hover:bg-slate-600 hover:text-slate-200 duration-300 p-2">
+            {SvgIcon && (
+              <SvgIcon className="w-10 h-10 text-primary bg-primary-content/90 rounded-full p-1" />
+            )}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Box;
---
+++ src/components/Box/__docs__/Box3.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Box3 from "../Box3";
+
+const meta: Meta<typeof Box3> = { title: "Box/Box3", component: Box3 };
+
+export default meta;
+type Story = StoryObj<typeof Box3>;
+
+export const Primary: Story = {
+  args: {
+    height: 330,
+    width: 250,
+    icon: "cleanRooms",
+    title: "Card Title",
+    text: "Using Lorem ipsum to focus attention on graphic elements in a webpage design proposal · One of the earliest examples of the Lorem ipsum placeholder text on 1960s advertising...",
+  },
+};
