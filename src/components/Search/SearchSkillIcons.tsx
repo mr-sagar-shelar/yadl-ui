af71c6a85daab2c7d76dfc76f3095f5f5daa96ab
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-02 22:24:56 +0530

    Added AWS Icons

--- src/components/Search/SearchSkillIcons.tsx
+++ src/components/Search/SearchSkillIcons.tsx
@@ -18,7 +18,7 @@ const SearchSkillIcons = (props: SearchSkillIconsProps) => {
       if (Icon) {
         // @ts-ignore
         return (
-          <div key={iconName}>{Icon && <Icon width={100} height={100} />}</div>
+          <div key={iconName}>{Icon && <Icon width={60} height={60} />}</div>
         );
       }
       return null;
@@ -27,7 +27,7 @@ const SearchSkillIcons = (props: SearchSkillIconsProps) => {
   }, []);
   return (
     <>
-      <div className="p-10 grid grid-cols-3 gap-6 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-10 2xl:grid-cols-12 overflow-auto w-full h-full">
+      <div className="p-10 grid grid-cols-5 gap-6 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-12 2xl:grid-cols-14 overflow-auto w-full h-full">
         {AllIcons}
       </div>
     </>
