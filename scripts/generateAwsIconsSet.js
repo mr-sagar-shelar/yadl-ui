c26bf7e58760d143ce268ccef63609bbefb28895
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-03 19:41:09 +0530

    Added AWS icons

--- scripts/generateAwsIconsSet.js
+++ scripts/generateAwsIconsSet.js
@@ -1,7 +1,9 @@
 import {
   writeComponentSkeleton,
   writeIndexFile,
+  toSpacedCase,
   toPascalCase,
+  toCamelCase,
 } from "./utils.js";
 import fs from "fs";
 import { transform } from "@svgr/core";
@@ -19,6 +21,15 @@ import { transform } from "@svgr/core";
       fs.readdir(folderName, (err, folders) => {
         folders.forEach((file) => {
           let fileName = file;
+          let originalFileName = file;
+          originalFileName = originalFileName.replaceAll("-", " ");
+          originalFileName = originalFileName.substring(0, fileName.length - 4);
+          originalFileName = originalFileName.replaceAll("Light", " ");
+          originalFileName = originalFileName.trim();
+          originalFileName = toSpacedCase(originalFileName);
+          originalFileName = originalFileName.trim();
+          originalFileName = originalFileName.replaceAll("  ", " ");
+
           fileName = fileName.replaceAll("-", "");
           fileName = fileName.replaceAll("&", "");
           fileName = fileName.replaceAll("Light", "");
@@ -33,37 +44,52 @@ import { transform } from "@svgr/core";
               // svgCode = svgCode.replaceAll("#F4F2ED", "var(--color-base-content)");
               // svgCode = svgCode.replace(/<!--([^;]*)-->/i, "");
               // console.log(`${folderName}/${fileName} \n\n${svgCode}`);
+
               try {
                 // console.log(`$$$$ Transforming ${fileName}`);
-                const componentCode = await transform(
-                  svgCode,
-                  {
-                    plugins: [
-                      "@svgr/plugin-svgo",
-                      "@svgr/plugin-jsx",
-                      "@svgr/plugin-prettier",
-                    ],
-                    icon: true,
-                    jsxRuntime: "automatic",
-                    typescript: true,
-                    memo: true,
-                    svgo: true,
-                  },
-                  { componentName: fileName },
-                );
+                // const componentCode = await transform(
+                //   svgCode,
+                //   {
+                //     plugins: [
+                //       "@svgr/plugin-svgo",
+                //       "@svgr/plugin-jsx",
+                //       "@svgr/plugin-prettier",
+                //     ],
+                //     icon: true,
+                //     jsxRuntime: "automatic",
+                //     typescript: true,
+                //     memo: true,
+                //     svgo: true,
+                //   },
+                //   { componentName: fileName },
+                // );
                 // console.log(`${componentCode}`);
                 let folderNameUpdated = currentFolder.replaceAll("-", "");
-                console.log(
-                  `./src/components/AWS/${folderNameUpdated}/${fileName}`,
+                // console.log(
+                //   `./src/components/AWS/${folderNameUpdated}/${fileName}`,
+                // );
+                const tempTitle = `AWS/${folderNameUpdated}`.replaceAll(
+                  "/",
+                  "",
                 );
 
-                writeComponentSkeleton(
-                  fileName,
-                  `./src/components/AWS/${folderNameUpdated}/${fileName}`,
-                  componentCode,
-                  `AWS/${folderNameUpdated}`,
-                  "https://aws-icons.com/",
+                const indexName = `${tempTitle}${fileName}`;
+                const newPayload = {
+                  name: originalFileName,
+                  icon: indexName,
+                };
+                // console.log(`${toCamelCase(indexName)}: "${indexName}" ,`);
+                console.log(
+                  `${toCamelCase(indexName)}: ${JSON.stringify(newPayload)} ,`,
                 );
+
+                // writeComponentSkeleton(
+                //   fileName,
+                //   `./src/components/AWS/${folderNameUpdated}/${fileName}`,
+                //   componentCode,
+                //   `AWS/${folderNameUpdated}`,
+                //   "https://aws-icons.com/",
+                // );
               } catch (error) {
                 console.log(`Error Transforming ${fileName}`);
                 console.error(error);
@@ -75,7 +101,7 @@ import { transform } from "@svgr/core";
                   },
                 );
               }
-              indexFileContents = `${indexFileContents}\nexport * from "./${fileName}";`;
+              // indexFileContents = `${indexFileContents}\nexport * from "./${fileName}";`;
             },
           );
         });
--- src/components/Search/SearchSkillIcons.tsx
+++ src/components/Search/SearchSkillIcons.tsx
@@ -1,12 +1,29 @@
-import { SkillIconNames, SkillIcons } from "@utils";
+import { SkillIcons, AwsIcons } from "@utils";
 import { useMemo } from "react";
 import * as ICONS from "../../index";
 import { memo } from "react";
 
 const SearchSkillIcons = () => {
-  console.log(Object.keys(SkillIconNames).length);
-  const AllIcons = useMemo(() => {
+  const SkillIconsComponent = useMemo(() => {
     const listItems = Object.values(SkillIcons).map((iconDetails) => {
+      // @ts-ignore
+      let Icon = ICONS[iconDetails.icon];
+
+      if (Icon) {
+        // @ts-ignore
+        return (
+          <div key={iconDetails.icon}>
+            {Icon && <Icon width={60} height={60} />}
+            {iconDetails.name}
+          </div>
+        );
+      }
+      return null;
+    });
+    return listItems;
+  }, []);
+  const AwsIconsComponent = useMemo(() => {
+    const listItems = Object.values(AwsIcons).map((iconDetails) => {
       // @ts-ignore
       let Icon = ICONS[iconDetails.icon];
 
@@ -25,8 +42,64 @@ const SearchSkillIcons = () => {
   }, []);
   return (
     <>
-      <div className="p-10 grid grid-cols-5 gap-6 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-12 2xl:grid-cols-14 overflow-auto w-full h-full">
-        {AllIcons}
+      <div className="tabs tabs-box p-5  w-full h-full">
+        <input
+          type="radio"
+          name="searchIconTabs"
+          className="tab"
+          aria-label="Skill Icons"
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">
+          <div className="grid grid-cols-5 gap-6 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-12 2xl:grid-cols-14 overflow-auto w-full h-full">
+            {SkillIconsComponent}
+          </div>
+        </div>
+        <input
+          type="radio"
+          name="searchIconTabs"
+          className="tab"
+          aria-label="GCP"
+          defaultChecked
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">GCP</div>
+
+        <input
+          type="radio"
+          name="searchIconTabs"
+          className="tab"
+          aria-label="AWS"
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">
+          <div className="grid grid-cols-5 gap-6 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-12 2xl:grid-cols-14 overflow-auto w-full h-full">
+            {AwsIconsComponent}
+          </div>
+        </div>
+
+        <input
+          type="radio"
+          name="searchIconTabs"
+          className="tab"
+          aria-label="Azure"
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">Azure</div>
+        <input
+          type="radio"
+          name="searchIconTabs"
+          className="tab"
+          aria-label="Themeisle"
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">
+          Themeisle
+        </div>
+        <input
+          type="radio"
+          name="searchIconTabs"
+          className="tab"
+          aria-label="Undraw"
+        />
+        <div className="tab-content bg-base-100 border-base-300 p-6">
+          Undraw
+        </div>
       </div>
     </>
   );
