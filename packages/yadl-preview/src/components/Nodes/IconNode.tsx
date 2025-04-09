834a900b934626de2a78b763274c015f73756c7a
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-09 11:51:44 +0530

    Added Icon node component for yadl-preview

--- packages/yadl-preview/src/components/Nodes/IconNode.tsx
+++ packages/yadl-preview/src/components/Nodes/IconNode.tsx
@@ -1,68 +1,80 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import {
   Handle,
   Position,
   NodeResizer,
-  // ResizeDragEvent,
-  // ResizeParamsWithDirection,
+  ResizeDragEvent,
+  ResizeParamsWithDirection,
 } from "@xyflow/react";
 import { IconNodeProps } from "yadl-core-package";
+import * as AWSIcons from "yadl-aws-icons";
+import { AwsIconNames } from "yadl-aws-icons";
+import * as AzureIcons from "yadl-azure-icons";
+import { AzureIconNames } from "yadl-azure-icons";
+import * as GCPIcons from "yadl-gcp-icons";
+import { GCPIconNames } from "yadl-gcp-icons";
+import * as SkillIcons from "yadl-skill-icons";
+import { SkillIconNames  } from "yadl-skill-icons";
+import * as ThemeisleIcons from "yadl-themeisle-icons";
+import { ThemeisleIconNames } from "yadl-themeisle-icons";
+import * as UndrawIcons from "yadl-undraw-icons";
+import { UndrawIconNames } from "yadl-undraw-icons";
 
 function IconNode(props: IconNodeProps) {
   const {
-    // data: { icon, width = 25, height = 25, category = "skill" },
+    data: { icon, width = 25, height = 25, category = "skill" },
     selected = false,
   } = props;
-  // const [currentWidth, setCurrentWidth] = useState<number>(width);
-  // const [currentHeight, setCurrentHeight] = useState<number>(height);
+  const [currentWidth, setCurrentWidth] = useState<number>(width);
+  const [currentHeight, setCurrentHeight] = useState<number>(height);
 
-  // let iconNamePresent: boolean = false;
-  // let Icon = null;
+  let iconNamePresent: boolean = false;
+  let Icon = null;
 
-  // switch (category) {
-  //   // case "aws":
-  //   //   iconNamePresent = AwsIconNames[icon] != undefined;
-  //   //   if (iconNamePresent) {
-  //   //     // @ts-ignore
-  //   //     Icon = AWSIcons[AwsIconNames[icon].icon] ?? null;
-  //   //   }
-  //   //   break;
-  //   case "gcp":
-  //     iconNamePresent = GCPIcons[icon] != undefined;
-  //     if (iconNamePresent) {
-  //       // @ts-ignore
-  //       Icon = ICONS[GCPIcons[icon].icon] ?? null;
-  //     }
-  //     break;
-  //   case "skill":
-  //     iconNamePresent = SkillIcons[icon] != undefined;
-  //     if (iconNamePresent) {
-  //       // @ts-ignore
-  //       Icon = ICONS[SkillIcons[icon].icon] ?? null;
-  //     }
-  //     break;
-  //   case "azure":
-  //     iconNamePresent = AzureIcons[icon] != undefined;
-  //     if (iconNamePresent) {
-  //       // @ts-ignore
-  //       Icon = ICONS[AzureIcons[icon].icon] ?? null;
-  //     }
-  //     break;
-  //   case "undraw":
-  //     iconNamePresent = UndrawIcons[icon] != undefined;
-  //     if (iconNamePresent) {
-  //       // @ts-ignore
-  //       Icon = ICONS[UndrawIcons[icon].icon] ?? null;
-  //     }
-  //     break;
-  //   case "themeisle":
-  //     iconNamePresent = ThemeisleIcons[icon] != undefined;
-  //     if (iconNamePresent) {
-  //       // @ts-ignore
-  //       Icon = ICONS[ThemeisleIcons[icon].icon] ?? null;
-  //     }
-  //     break;
-  // }
+  switch (category) {
+    case "aws":
+      iconNamePresent = AwsIconNames[icon] != undefined;
+      if (iconNamePresent) {
+        // @ts-ignore
+        Icon = AWSIcons[AwsIconNames[icon].icon] ?? null;
+      }
+      break;
+    case "gcp":
+      iconNamePresent = GCPIconNames[icon] != undefined;
+      if (iconNamePresent) {
+        // @ts-ignore
+        Icon = GCPIcons[GCPIconNames[icon].icon] ?? null;
+      }
+      break;
+    case "skill":
+      iconNamePresent = SkillIconNames[icon] != undefined;
+      if (iconNamePresent) {
+        // @ts-ignore
+        Icon = SkillIcons[SkillIconNames[icon].icon] ?? null;
+      }
+      break;
+    case "azure":
+      iconNamePresent = AzureIconNames[icon] != undefined;
+      if (iconNamePresent) {
+        // @ts-ignore
+        Icon = AzureIcons[AzureIconNames[icon].icon] ?? null;
+      }
+      break;
+    case "undraw":
+      iconNamePresent = UndrawIconNames[icon] != undefined;
+      if (iconNamePresent) {
+        // @ts-ignore
+        Icon = UndrawIcons[UndrawIconNames[icon].icon] ?? null;
+      }
+      break;
+    case "themeisle":
+      iconNamePresent = ThemeisleIconNames[icon] != undefined;
+      if (iconNamePresent) {
+        // @ts-ignore
+        Icon = ThemeisleIcons[ThemeisleIconNames[icon].icon] ?? null;
+      }
+      break;
+  }
 
   return (
     <label htmlFor="properties-drawer">
@@ -71,22 +83,19 @@ function IconNode(props: IconNodeProps) {
           console.log("Clicked");
         }}
       >
-        {/* {iconNamePresent && (
+        {iconNamePresent && (
           <Icon width={currentWidth} height={currentHeight} />
-        )} */}
-        Icon Node
+        )}
         <NodeResizer
           color="#ff0071"
           isVisible={selected}
-          // minWidth={50}
-          // minHeight={50}
-          // onResize={(
-          //   _event: ResizeDragEvent,
-          //   params: ResizeParamsWithDirection,
-          // ) => {
-          //   setCurrentWidth(params.width);
-          //   setCurrentHeight(params.height);
-          // }}
+          onResize={(
+            _event: ResizeDragEvent,
+            params: ResizeParamsWithDirection,
+          ) => {
+            setCurrentWidth(params.width);
+            setCurrentHeight(params.height);
+          }}
           keepAspectRatio
         />
 
