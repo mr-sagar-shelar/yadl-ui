d8baef3254494b3894143cdbed5a69a129db4edb
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-11 19:58:43 +0530

    Completed hello world of editor

--- packages/yadl-editor/src/components/Editor/Editor.tsx
+++ packages/yadl-editor/src/components/Editor/Editor.tsx
@@ -1,4 +1,30 @@
-import { memo } from "react";
-const Editor = () => <>This is editor</>;
-const Memo = memo(Editor);
-export default Memo;
+import {
+  MonacoEditorLanguageClientWrapper,
+  WrapperConfig,
+} from "monaco-editor-wrapper";
+import { MonacoEditorReactComp } from "@typefox/monaco-editor-react";
+
+const wrapperConfig: WrapperConfig = {
+  $type: "extended",
+  htmlContainer: document.getElementById("monaco-editor-root")!,
+  editorAppConfig: {
+    codeResources: {
+      modified: {
+        uri: "/workspace/hello.py",
+        text: 'print("Hello, World!")',
+      },
+    },
+  },
+};
+const Editor = () => (
+  <>
+    <MonacoEditorReactComp
+      wrapperConfig={wrapperConfig}
+      style={{ height: "100%", width: "100%" }}
+      onLoad={(wrapper: MonacoEditorLanguageClientWrapper) => {
+        // use the wrapper to get access to monaco-editor or the languageclient
+      }}
+    />
+  </>
+);
+export default Editor;
