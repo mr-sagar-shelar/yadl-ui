57f4f3f5b7c620441ec30233890f8b2615fbd06c
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-14 20:09:44 +0530

    Added gcp, azure, skill, themeisle and undraw icons support for yadl language

--- packages/yadl-editor-core/yadl-lang/static/setupClassic.js
+++ packages/yadl-editor-core/yadl-lang/static/setupClassic.js
@@ -1,32 +1,48 @@
-import { addMonacoStyles, defineUserServices, MonacoEditorLanguageClientWrapper } from './bundle/index.js';
+import {
+  addMonacoStyles,
+  defineUserServices,
+  MonacoEditorLanguageClientWrapper,
+} from "./bundle/index.js";
 import monarchSyntax from "../syntaxes/yadl.monarch.js";
-import { configureWorker } from './setup.js';
+import { configureWorker } from "./setup.js";
 
-addMonacoStyles('monaco-editor-styles');
+addMonacoStyles("monaco-editor-styles");
 
 export const setupConfigClassic = () => {
-    return {
-        wrapperConfig: {
-            serviceConfig: defineUserServices(),
-            editorAppConfig: {
-                $type: 'classic',
-                languageId: 'yadl',
-                code: `// YADL is running in the web!`,
-                useDiffEditor: false,
-                languageExtensionConfig: { id: 'langium' },
-                languageDef: monarchSyntax,
-                editorOptions: {
-                    'semanticHighlighting.enabled': true,
-                    theme: 'vs-dark'
-                }
-            }
+  return {
+    wrapperConfig: {
+      serviceConfig: defineUserServices(),
+      editorAppConfig: {
+        $type: "classic",
+        languageId: "yadl",
+        code: `// YADL is running in the web!
+aws-icon aWSVRARSumerian
+
+azure-icon azureComputeComputeFleet
+
+gcp-icon gCPAiPlatform
+
+skill-icon skillIconUbuntu
+
+themeisle-icon themeisle104
+
+undraw-icon undrawYear2022 
+`,
+        useDiffEditor: false,
+        languageExtensionConfig: { id: "langium" },
+        languageDef: monarchSyntax,
+        editorOptions: {
+          "semanticHighlighting.enabled": true,
+          theme: "vs-dark",
         },
-        languageClientConfig: configureWorker()
-    };
+      },
+    },
+    languageClientConfig: configureWorker(),
+  };
 };
 
 export const executeClassic = async (htmlElement) => {
-    const userConfig = setupConfigClassic();
-    const wrapper = new MonacoEditorLanguageClientWrapper();
-    await wrapper.initAndStart(userConfig, htmlElement);
+  const userConfig = setupConfigClassic();
+  const wrapper = new MonacoEditorLanguageClientWrapper();
+  await wrapper.initAndStart(userConfig, htmlElement);
 };
